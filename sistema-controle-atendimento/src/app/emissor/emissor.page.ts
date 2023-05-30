3d5d9f349a22d16d917383645889c64ef361eba5
Lucas Mafra <lucas.lima.nave@gmail.com>
2023-05-30 01:50:08 -0300

    Página de Emissor concluída

--- sistema-controle-atendimento/src/app/emissor/emissor.page.ts
+++ sistema-controle-atendimento/src/app/emissor/emissor.page.ts
@@ -6,8 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emissor.page.scss'],
 })
 export class EmissorPage implements OnInit {
+  // Array que irá salvar as senhas geradas pelo botão, será utilizada o esquema chave → valor,
+  // sendo, respectivamente, cpf e senha
+  senhas: {chave: number, valor: any}[] = [];
 
-  constructor() { }
+  // Pegar a data atual
+  dataAtual = new Date();
+  ano = this.dataAtual.getFullYear(); 
+  mes = this.dataAtual.getMonth() + 1;
+  dia = this.dataAtual.getDate();
+
+  // Contadores para a sequência das senhas
+  SqSp: number = 0;
+  SqSe: number = 0;
+  SqSg: number = 0;
+
+  // Variavel para salvar o cpf
+  cpf: number = 0;
+
+  // Métodos para gerar as senhas
+  gerarSenhasSP(){
+    this.senhas.push({chave: this.cpf, valor: this.ano+ "/" + this.mes + "/" + this.dia + "-SP" + this.SqSp})
+    this.cpf = 0;
+    this.SqSp++;
+  }
+  gerarSenhasSE(){
+    this.senhas.push({chave: this.cpf, valor: this.ano+ "/" + this.mes + "/" + this.dia + "-SE" + this.SqSe})
+    this.cpf = 0;
+    this.SqSe++;
+  }
+  gerarSenhasSG(){
+    this.senhas.push({chave: this.cpf, valor: this.ano+ "/" + this.mes + "/" + this.dia + "-SG" + this.SqSg})
+    this.cpf = 0;
+    this.SqSg++;
+  }
+
+  constructor() {
+   }
 
   ngOnInit() {
   }
